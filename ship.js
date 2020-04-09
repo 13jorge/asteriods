49030ea40c7689ca430cb775264162356b5610a7
13jorge <54521719+13jorge@users.noreply.github.com>
2020-04-08 19:24:32 -0700

    sketch

--- ship.js
+++ ship.js
@@ -1,33 +1,3 @@
-let ship;
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ship = new Ship();
-}
-
-function draw() {
-  background(0);
-  ship.render();
-  ship.turn();
-  ship.update();
-  ship.edges();
-}
-
-function keyReleased(){
-  ship.setRotation(0);
-  ship.boosting(false);
-
-}
-
-function keyPressed(){
-  if (keyCode == RIGHT_ARROW){
-    ship.setRotation(0.1);
-  }else if(keyCode == LEFT_ARROW){
-    ship.setRotation(-0.1);
-  }else if(keyCode == UP_ARROW){
-    ship.boosting(true);
-  }
-}
-
 function Ship(){
   this.pos = createVector(width/2,height/2);
   this.r = 20;
